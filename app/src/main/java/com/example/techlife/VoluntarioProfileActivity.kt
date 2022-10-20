e96a1467d38de7d800e1aba1c1552999b7f040ec
Erick.BH <erickalonsobh@gmail.com>
2022-10-20 15:11:45 -0500

    Added Volunteer profile

---
+++ app/src/main/java/com/example/techlife/VoluntarioProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.techlife
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+class VoluntarioProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.voluntario_profile_activity)
+
+    }
+}
